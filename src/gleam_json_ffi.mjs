08ce779eca6cb932dd4e9884937e4b782deda0fe
mrkutly <mark.sauer.utley@gmail.com>
2022-05-22 13:31:43 -0400

    fix json converters

--- src/gleam_json_ffi.mjs
+++ src/gleam_json_ffi.mjs
@@ -1,65 +1,6 @@
-import { Ok, Error, CustomType } from './gleam.mjs'
+import { Ok, Error, toList } from './gleam.mjs'
 import { bit_string_to_string } from '../../gleam_stdlib/dist/gleam_stdlib.mjs'
-
-export class DecodeError extends CustomType {
-  get __gleam_prelude_variant__() {
-    return "DecodeError";
-  }
-}
-
-export class UnexpectedEndOfInput extends CustomType {
-  static isInstance(err) {
-    return err.message === 'Unexpected end of JSON input'
-  }
-
-  get __gleam_prelude_variant__() {
-    return "UnexpectedEndOfInput"
-  }
-}
-
-export class UnexpectedByte extends CustomType {
-  static regex = /Unexpected token (.) in JSON at position (\d+)/
-
-  static isInstance(err) {
-    return this.regex.test(err)
-  }
-
-  constructor(err) {
-    super()
-    const match = UnexpectedByte.regex.exec(err.message)
-    this.byte = "0x" + match[1].charCodeAt(0).toString(16).toUpperCase()
-    this.position = Number(match[2])
-  }
-
-  get __gleam_prelude_variant__() {
-    return `UnexpectedByte`;
-  }
-}
-
-export class UnexpectedSequence extends CustomType {
-  static isInstance(err) {
-    return false
-  }
-
-  get __gleam_prelude_variant__() {
-    return "UnexpectedSequence";
-  }
-}
-
-export class UnexpectedFormat extends CustomType {
-  static isInstance(err) {
-    return false
-  }
-
-  constructor(decodeErrs) {
-    super()
-    this[0] = decodeErrs
-  }
-
-  get __gleam_prelude_variant__() {
-    return "UnexpectedFormat";
-  }
-}
+import { UnexpectedByte, UnexpectedEndOfInput, UnexpectedFormat } from './gleam/json.mjs'
 
 export function json_to_string(json) {
   return JSON.stringify(json)
@@ -77,9 +18,22 @@ export function do_null() {
   return null
 }
 
-export function identity(x) {
-  return x
+export function string(x) {
+  return json_to_string(x)
 }
+
+export function int(x) {
+  return parseInt(json_to_string(x), 10)
+}
+
+export function float(x) {
+  return parseFloat(json_to_string(x), 10)
+}
+
+export function bool(x) {
+  return Boolean(x)
+}
+
 
 export function decode(bit_string) {
   const stringResult = bit_string_to_string(bit_string)
@@ -88,18 +42,29 @@ export function decode(bit_string) {
     const result = JSON.parse(stringResult[0])
     return new Ok(result)
   } catch (err) {
-    return getJsonDecodeError(err)
+    return new Error(getJsonDecodeError(err))
   }
 }
 
 function getJsonDecodeError(stdErr) {
-  const ErrClass = [
-    UnexpectedByte,
-    UnexpectedEndOfInput,
-    UnexpectedFormat,
-    UnexpectedSequence,
-  ].find((klass) => klass.isInstance(stdErr))
+  if (isUnexpectedByte(stdErr)) return new toUnexpectedByteError(stdErr)
+  if (isUnexpectedEndOfInput(stdErr)) return new UnexpectedEndOfInput()
+  return undefined
+}
 
-  if (ErrClass) return new Error(new ErrClass(stdErr))
-  else return new Error()
+function isUnexpectedEndOfInput(err) {
+  return err.message === 'Unexpected end of JSON input'
+}
+
+const unexpectedByteRegex = /Unexpected token (.) in JSON at position (\d+)/
+
+function isUnexpectedByte(err) {
+  return unexpectedByteRegex.test(err)
+}
+
+function toUnexpectedByteError(err) {
+  const match = unexpectedByteRegex.exec(err.message)
+  const byte = "0x" + match[1].charCodeAt(0).toString(16).toUpperCase()
+  const position = Number(match[2])
+  return new UnexpectedByte(byte, position)
 }
